b21f2c0b8b34f77ea562f355006377aa8aae5091
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-27 09:40:31 +0100

    setting up react via cdn

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-class User{
-    constructor(name, age, gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-    login(){
-        console.log(this.name, "login successful");
-        return this;
-    }
-    logout(){
-        console.log(this.name, "logout successful");
-        return this;
-    }
-}
+ReactDOM.render(<h1>Hello Israel</h1>, document.getElementById('root'))
 
 
-class Admin extends User{
-    deleteUser(user){
-        users = users.filter(u => {
-            return u.email != user.email
-        })
-    }
-}
 
 
-var userOne = new User ("emma", 20, "male");
-var userTwo = new User ("israel", 22, "male");
 
-var users = [userOne, userTwo];
+
+
+
+
+
+
+
+
+
+// class User{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+//     login(){
+//         console.log(this.name, "login successful");
+//         return this;
+//     }
+//     logout(){
+//         console.log(this.name, "logout successful");
+//         return this;
+//     }
+// }
+
+
+// class Admin extends User{
+//     deleteUser(user){
+//         users = users.filter(u => {
+//             return u.email != user.email
+//         })
+//     }
+// }
+
+
+// var userOne = new User ("emma", 20, "male");
+// var userTwo = new User ("israel", 22, "male");
+
+// var users = [userOne, userTwo];
 
 
 // Javascript Prototype Construction Function Model
