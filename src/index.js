1c7a5d97b88958f709e709ccbec613f60e436682
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-28 12:34:10 +0100

    create composable components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import react from "react"
 import ReactDOM from "react-dom"
 
+function Header() {
+  return (
+    <header>
+          <nav>
+            <img src="./logo512.png" width="100" />
+          </nav>
+      </header>
+  )
+}
+
+function MainContent() {
+  return (
+
+    <div>
+        <h1>Reasons I'm Excited To Learn React</h1>
+        <ul>
+          <li>Because it is user friendly</li>
+          <li>Because i want to improve my web development skills</li>
+        </ul>
+    </div>
+    
+  )
+}
+
+function Footer() {
+    return (
+
+      <footer>
+          <p> &copy; 2023 Robert's development. All Rights reserved.</p>
+      </footer>
+
+  )
+}
+
 function MyPage() {
 
   return(
     <div>
-      <h1>Why I Want To Learn React</h1>
-      <ul>
-        <li>Because it is user friendly</li>
-        <li>Because i want to improve my web development skills</li>
-      </ul>
+        <Header />
+        <MainContent />
+        <Footer />
+        
+        
     </div>
   )
 
