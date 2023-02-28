1db25f5f5df40b11075f7331ccd87bc1c80b848e
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-28 11:24:45 +0100

    add git to react folder

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import react from "react"
+import ReactDOM  from "react-dom"
+
+const element = (
+
+  
+<div>
+
+  <div>
+      <img src="./logo512.png" width="100"/>
+  </div>
+
+  <div>
+    <h1>Fun Facts About React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Valke</li>
+      <li>Has over well 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousand of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+  
+</div>
+
+)
+
+ReactDOM.render(element, document.getElementById("root"))
