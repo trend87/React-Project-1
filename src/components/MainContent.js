1df8a60de3581cce541830ce8991b53d98cc39eb
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-28 15:01:08 +0100

    change ol to ul and add more li items

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,11 +4,14 @@ function MainContent() {
     return (
   
       <div className="mainContent">
-          <h1>Reasons I'm Excited To Learn React</h1>
-          <ol>
-            <li>Because it is user friendly</li>
-            <li>Because i want to improve my web development skills</li>
-          </ol>
+          <h1>Fun facts about React</h1>
+          <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
       </div>
       
     )
