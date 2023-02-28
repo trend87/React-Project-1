d8d06f0446e0a64bd5186d3b6032728c654d1f8e
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-28 13:24:03 +0100

    removed components into its respective files

--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 import react from "react"
 import ReactDOM from "react-dom"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 import "./index.css"
-
-function Header() {
-  return (
-    <header >
-          <nav className="nav">
-            <img src="./logo512.png" className="logo" />
-            <ul className="nav-items" >
-              <li>Pricing</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-      </header>
-  )
-}
-
-function MainContent() {
-  return (
-
-    <div className="mainContent">
-        <h1>Reasons I'm Excited To Learn React</h1>
-        <ol>
-          <li>Because it is user friendly</li>
-          <li>Because i want to improve my web development skills</li>
-        </ol>
-    </div>
-    
-  )
-}
-
-function Footer() {
-    return (
-
-      <footer className="footer">
-          <p> &copy; 2023 Robert's development. All Rights reserved.</p>
-      </footer>
-
-  )
-}
 
 function MyPage() {
 
