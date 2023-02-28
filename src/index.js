e9ee9008237a782cb6ed388557c2d3632f75fdb2
Effiong Israel Robert <israelrobert@gmail.com>
2023-02-28 12:03:03 +0100

    create first component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import react from "react"
-import ReactDOM  from "react-dom"
+import ReactDOM from "react-dom"
 
-const element = (
+function MyPage() {
 
-  
-<div>
+  return(
+    <div>
+      <h1>Why I Want To Learn React</h1>
+      <ul>
+        <li>Because it is user friendly</li>
+        <li>Because i want to improve my web development skills</li>
+      </ul>
+    </div>
+  )
 
-  <div>
-      <img src="./logo512.png" width="100"/>
-  </div>
+}
 
-  <div>
-    <h1>Fun Facts About React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Valke</li>
-      <li>Has over well 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousand of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-  
-</div>
-
-)
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(<MyPage />, document.getElementById("root"))
